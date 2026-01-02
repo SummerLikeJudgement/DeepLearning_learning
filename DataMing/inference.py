618f2c54b451efe5fe80d46442f624ec7aeb2f4b
summer <summer35174@gmail.com>
2026-01-02 16:21:13 +0800

    inference update

--- DataMing/inference.py
+++ DataMing/inference.py
@@ -29,8 +29,8 @@ warnings.filterwarnings('ignore')
 
 # Append inference specific configuration
 class Config(TrainConfig):
-    DATA_DIR = '/kaggle/input/csiro-biomass'
-    MODEL_DIR = '/kaggle/input/csiro/pytorch/default/1' 
+    DATA_DIR = "dataset/csiro-biomass'"
+    MODEL_DIR = "checkpoints"
     
     FOLDS_TO_INFER = [0, 1, 2, 3, 4] 
 
